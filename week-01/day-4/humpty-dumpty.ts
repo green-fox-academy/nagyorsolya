19b997770df691cc6f48d01fcef1b8692608332b
Orsi Nagy <orsolya.n91@gmail.com>
2019-01-17 14:37:41 +0100

    Created humpty-dumpty.ts file

---
+++ week-01/day-4/humpty-dumpty.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Modify this program to console.log Humpty Dumpty riddle correctly
+
+var third = 'All the king\'s horses and all the king\'s men'
+var second = 'Humpty Dumpty had a great fall.'
+var first = 'Humpty Dumpty sat on a wall,'
+var fourth = 'Couldn\'t put Humpty together again.'
+
+console.log (first)
+console.log (second)
+console.log  (third)
+console.log (fourth)
