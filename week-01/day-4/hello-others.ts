48bd3e8c93c59f21070a3668a89f6f8fcc03f348
Orsi Nagy <orsolya.n91@gmail.com>
2019-01-17 14:51:40 +0100

    Created hello-others.ts file

---
+++ week-01/day-4/hello-others.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+console.log('Hello, Judit!');
+console.log('Hello, Max!');
+console.log('Hello, Adam!');
