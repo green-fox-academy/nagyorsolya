1dd283ed6a4017de40cb98d0327f193c31f87fba
Orsi Nagy <orsolya.n91@gmail.com>
2019-01-17 16:30:51 +0100

    Created BMI.ts file

---
+++ week-01/day-4/bmi.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+//To work out your BMI:
+//divide your weight in kilograms (kg) by your height in metres (m)
+//then divide the answer by your height again to get your BMI.
+
+var BMI = (massInKg/heightInM)/heightInM
+console.log (BMI)
