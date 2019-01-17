abd60da272311b1d378ee4fea495924c707f041e
Orsi Nagy <orsolya.n91@gmail.com>
2019-01-17 15:04:52 +0100

    Created introduce-yourself.ts file.

---
+++ week-01/day-4/introduce-yourself.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+var myname = 'Nagy Orsi'
+var myage = '27'
+var myheight:number = 1.63 
+
+console.log (myname)
+console.log (myage)
+console.log (myheight + ' meters')
