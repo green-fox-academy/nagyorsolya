0275d410a1369102ae1b5bb32ba2458cd53b12a3
Orsi Nagy <orsolya.n91@gmail.com>
2019-01-17 16:41:48 +0100

    Created define-basic-info.ts file

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var myname: string = "Orsi Nagy"
+var myage: number = 27
+var myheight: number = 1.62
+var marriedstatus: boolean = false
+console.log (myname);
+console.log (myage);
+console.log (myheight);
+console.log (marriedstatus); 
