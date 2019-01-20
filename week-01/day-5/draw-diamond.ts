8154dc003641780c660a2413af9ca4f5fe22e49e
Orsi Nagy <orsolya.n91@gmail.com>
2019-01-20 13:45:53 +0100

    Created draw-pyramid.ts and draw-diamond.ts file

---
+++ week-01/day-5/draw-diamond.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+let lineCount: number = 5;
+let m: number = lineCount-1; //This tells us how tall the pyramid should be
+
+//The k cycle runs the cycles from 0-3
+for (let k:number = 0; k<m+1;k++) {
+    let space: string = "";
+    let spacekszama: number = m-k;
+    //The z cycle runs the cycles and subtracts the height from the current cycle showing how man 0s there should be (increases the number of zeros subtracted from the lines)
+    for (let z:number = 0; z<spacekszama;z++) {
+        space=space+" ";
+    }
+    //The i cycle runs the cycles and adds two more stars to the lines in each round 
+    let csillag:string = "";
+    let csillagokszama:number = (k*2)+1;
+    for (let i: number = 0; i<csillagokszama; i++) {
+        csillag=csillag+"*";
+    }
+    console.log(space+csillag);
+}
+for (let k:number = m; k >=0;k--) {
+    let space: string = "";
+    let spacekszama: number = m-k;
+    for (let z:number = 0; z<spacekszama;z++) {
+        space=space+" ";
+    }
+    let csillag:string = "";
+    let csillagokszama:number = (k*2)+1;
+    for (let i: number = 0; i<csillagokszama; i++) {
+        csillag=csillag+"*";
+    }
+    console.log(space+csillag);
+}
