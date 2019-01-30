00ea2b48be8c9ca2c5725a5da9e9322d3056f4f3
Orsi Nagy <orsolya.n91@gmail.com>
2019-01-30 14:44:00 +0100

    Created a couple of more files

---
+++ week-03/day-3/counter.ts
@@ -0,0 +1,33 @@
+import { inherits } from "util";
+
+/*Create a Counter class
+which has an integer property
+when creating it should have a default value 0 or we can specify it when creating
+we can add(number) to increase the counter's value by a whole number
+or we can add() without parameters just increasing the counter's value by one
+and we can get() the current value as string
+also we can reset() the value to the initial value*/
+
+class Counter {
+  integer: number = 0;
+  initialValue = 0;
+
+  constructor(a) {
+    this.integer = a;
+    this.initialValue = a;
+  }
+  add() {
+   this.integer++;
+   return this.integer;
+  }
+  get() {
+    return this.integer.toString();
+  }
+  reset() {
+   return this.integer = this.initialValue;
+}
+}
+
+let abc = new Counter (6);
+abc.get();
+
---
+++ week-03/day-3/sharpie.ts
@@ -0,0 +1,21 @@
+/*Create Sharpie class
+We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+When creating one, we need to specify the color and the width
+Every sharpie is created with a default 100 as inkAmount
+We can use() the sharpie objects
+which decreases inkAmount*/
+
+class Sharpie {
+  color: string;
+  width: number;
+  inkAmount: number = 100;
+
+  constructor (a,b) {
+this.color = a;
+this.width = b;
+  }
+  use () {
+    this.inkAmount--;
+  }  
+}
+
