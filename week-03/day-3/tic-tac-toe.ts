83037f9cdf9c40f1f216765de26eaa24d34463b7
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-03 14:01:47 +0100

    Goddamn tic-tac-toe

--- week-03/day-3/tic-tac-toe.ts
+++ week-03/day-3/tic-tac-toe.ts
@@ -1,16 +1,109 @@
+'use strict';
+export{}
+
+let fs = require('fs');
+
 // Write a function that takes a filename as a parameter
 // The file contains an ended Tic-Tac-Toe match
 // We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
 // Return "X", "O" or "Draw" based on the input file
 
-//First I need to convert the .txt files toa variable and "upload" them to the multidimensional array
+//First I need to convert the .txt files to a variable and "upload" them to the multidimensional array
+/*let content: string = "";
+let contentarray: string [] = [];
+let newarray: any [][] = [];
+content = fs.readFileSync("win-o.txt", "utf-8");
+contentarray = content.split("");
+contentarray.splice(3,2);
+contentarray.splice(6,2);
+let size = 3;
+
+for (let i = 0; i < size; i++) {
+  newarray[i] = [];
+  for (let j = 0; j < size; j++) {
+      newarray[i][j] = contentarray[i+j];
+      if (i === 1) {
+        newarray[i][j] = contentarray[i+j+2];
+      }
+      else if (i === 2) {
+        newarray[i][j] = contentarray[i+j+4];
+      }
+  }
+}; */
 
 //Second, we need to iterate through the arrays and check the elements
-function ticTacResult() {
-  for (let i = 0; i < amobaarray.length; i++)
-    for (let j = 0; j < a[j].length; i++)
 
-    // Then we need to establish if statements for the different scenarios
+function ticTacResult(inputFile) {
+
+  let content: string = "";
+  let contentarray: string [] = [];
+  let inputArray: any [][] = [];
+  content = fs.readFileSync(inputFile, "utf-8");
+  contentarray = content.split("");
+  contentarray.splice(3,2);
+  contentarray.splice(6,2);
+  let size = 3;
+  
+  for (let i = 0; i < size; i++) {
+    inputArray[i] = [];
+    for (let j = 0; j < size; j++) {
+      inputArray[i][j] = contentarray[i+j];
+        if (i === 1) {
+          inputArray[i][j] = contentarray[i+j+2];
+        }
+        else if (i === 2) {
+          inputArray[i][j] = contentarray[i+j+4];
+        }
+    }
+  };
+
+  let isThereAWinner: boolean = false;
+
+  for (let i = 0; i < inputArray.length; i++) {
+             //This is for the horizontal lines
+        if (inputArray[i][0] === inputArray[i][1] && inputArray[i][0] === inputArray [i][2] && inputArray[i][0] === "X") {
+          isThereAWinner = true;
+          console.log("X wins!");
+        }
+        else if (inputArray[i][0] === inputArray[i][1] && inputArray[i][0] === inputArray [i][2] && inputArray[i][0] === "O") {
+          isThereAWinner = true;
+          console.log("O wins!");
+        }
+        // This is for the vertical lines
+        if (inputArray[0][i] === inputArray[1][i] && inputArray[0][i] === inputArray[2][i] && inputArray[0][i] === "X") {
+          isThereAWinner = true;
+          console.log("X wins!");
+        } 
+        else if (inputArray[0][i] === inputArray[1][i] && inputArray[0][i] === inputArray[2][i] && inputArray[0][i] === "O") {
+          isThereAWinner = true;
+          console.log("O wins!");
+        }
+        //This is for the diagonals
+        if (i === 0 && inputArray[i][0] === inputArray[i+1][1] && inputArray[i][0] === inputArray[i+2][2] && inputArray[i][0] === "X") {
+          isThereAWinner = true;
+          console.log("X wins!");
+        }
+        else if (i === 0 && inputArray[i][0] === inputArray[i+1][1] && inputArray[i][0] === inputArray[i+2][2] && inputArray[i][0] === "O") {
+          isThereAWinner = true;
+          console.log("O wins!");
+        }
+        if (i === 0 && inputArray[i][2] === inputArray[i+1][i+1] && inputArray[i][2] === inputArray[2][i] && inputArray[i][2] === "X") {
+          isThereAWinner = true;
+          console.log("X wins!");
+        }
+        else if (i === 0 && inputArray[i][2] === inputArray[i+1][i+1] && inputArray[i][2] === inputArray[2][i] && inputArray[i][2] === "O") {
+          isThereAWinner = true;
+          console.log("O wins!");
+        }
+    }
+    if (!isThereAWinner) {
+      console.log("It's a draw!");
+    }
+  };
+
+  ticTacResult("draw.txt");
+
+   /* // Then we need to establish if statements for the different scenarios
     //This will be for the vertical lines
       if (a[i][j] === a[i][j + 1] && a[j] === a[j + 2] && a[j] === "x") {
         
@@ -30,4 +123,4 @@ console.log(ticTacResult('win-x.txt'))
 // Should print "X"
 
 console.log(ticTacResult('draw.txt'))
-// Should print "Draw"
+// Should print "Draw" */
