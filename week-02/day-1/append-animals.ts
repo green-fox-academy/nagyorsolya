20b5113c5f5f87c290a57adf14f6b74380f9d9f7
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-07 19:24:20 +0100

    Finished append-animals task

---
+++ week-02/day-1/append-animals.ts
@@ -0,0 +1,25 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string[] = ["koal", "pand", "zebr"];
+
+/*let animals1 = animals.forEach(function (value) {
+  return value += "a";
+})
+
+console.log(animals1); */
+
+let animals2 = animals.map(function (item) {
+  return item += "a";
+})
+
+console.log(animals2);
+
+//It's not working with forEach
+
+/*let animals1 = animals.forEach(function (value) {
+  return value += "a";
+})
+
+console.log(animals1); */
