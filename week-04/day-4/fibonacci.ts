0e95f4c0cf842a79c8c8e42abb0a5710507b0da2
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-07 17:09:30 +0100

    Created fibonacci task

---
+++ week-04/day-4/fibonacci.ts
@@ -0,0 +1,14 @@
+//Write a function that computes a member of the fibonacci sequence by a given index
+//Create tests that covers all types of input (like in the previous workshop exercise)
+//The function should return the fibonacci value of the the input index, so if you input 2, it should return the 3rd element of the fibonacci sequence
+// [0,1,1,2,3,5,8...]
+
+export function fibonacci (inputIndex: number) {
+let fibonacciArray: number [] = [0,1];
+for (let i = 2; i < inputIndex; i++) {
+  fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2];
+}
+return fibonacciArray[inputIndex-1];
+};
+
+console.log(fibonacci(3));
