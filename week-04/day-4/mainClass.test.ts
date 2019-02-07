2c4908382cc1e1aaa10a2a91cc8b527ff1f9c46b
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-07 17:18:56 +0100

    Tested fibonacci, it's working

--- week-04/day-4/mainClass.test.ts
+++ week-04/day-4/mainClass.test.ts
@@ -1,16 +1,24 @@
 //import {Apple} from './apples'
 //import {Sum} from './sum'
 //import {isAnagram} from './anagram';
-import {dictionary} from './count-letters'
+//import {dictionary} from './count-letters'
+import {fibonacci} from './fibonacci'
 
 let test = require('tape');
 
-test('Get the occurence of the letters', (t) => {
+test('Get the fibonacci number of the input index', (t) => {
+  let testIndex: number = 3;
+
+  t.deepEqual(fibonacci(testIndex), 1);
+  t.end();
+})
+
+/*test('Get the occurence of the letters', (t) => {
   let testString: string = "alma";
   
   t.deepEqual(dictionary(testString), { a: 2, l: 1, m: 1 })
   t.end();
-});
+});*/
 
 /*test('Get the name field of class', (t) => {
   let newApple = new Apple ("green");
