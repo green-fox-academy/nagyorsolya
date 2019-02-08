a9f1842a6ec8812ece9929e0f7493e03d72e6611
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-08 16:34:05 +0100

    Made some progress with the todo-app

--- week-04/day-5/todo-app-main.ts
+++ week-04/day-5/todo-app-main.ts
@@ -1,19 +1,33 @@
 'use strict';
 
-let readlineSync = require('readline-sync');
+class Todo {
+  name: string;
+  isCompleted: boolean = false;
+  constructor(newName: string) {
+    this.name = newName;
+  }
+  complete() {
+    this.isCompleted = true;
+  }
+};
+
+let task1 = new Todo ("Walk the dog");
+let task2 = new Todo ("Buy Milk");
+let task3 = new Todo ("Do homework");
+
+/*let readlineSync = require('readline-sync');
 let fs = require('fs');
 let content: string = fs.readFileSync("todolist.txt", "utf-8")
 let goodContent: any [] = content.split("\n")
-let taskList: object [] = [];
 
-function removeChars (array: any) {
-  let something: any [] = [];
-for (let i = 0; i < array.length; i++) {
-something[i] = array[i].replace("\r", "");
+function removeChars(array: any) {
+  let something: any[] = [];
+  for (let i = 0; i < array.length; i++) {
+    something[i] = array[i].replace("\r", "");
+  }
+  return something;
 }
-return something;
-}
-goodContent = removeChars(goodContent);
+goodContent = removeChars(goodContent); */
 
 let input1: string = readlineSync.question(
   'Welcome to my todo app, what would you like to add today? '
@@ -35,24 +49,32 @@ let input2: string = readlineSync.question(
   ''
 );
 if (input2 === "-l") {
-  goodContent.forEach(function(item: string,index: number) {
-    console.log(index + " [ ] ", item);
+  goodContent.forEach(function (item: string, index: number) {
+    console.log(index - 1 + " [ ] ", item);
   })
 }
 else if (input2 === "-a") {
   let input3: string = readlineSync.question(
     'Please type your new task to be added to the list:'
   );
-  goodContent.push(input3);
+  let newItem = new Todo(input3)
+  goodContent.push(newItem.name);
   fs.appendFileSync("todolist.txt", "\n" + goodContent);
 }
-/*else if (input2[0] === "-" && input2[1] === "a") {
- let splitTodo = input2.substring(3);
- console.log(splitTodo);
- goodContent.push(splitTodo);
- console.log(goodContent);
- fs.appendFileSync("todolist.txt", "\n" + goodContent);
-}*/
+else if (input2 === "-c") {
+  console.log('Please write the number of the task to complete:');
+
+  let input4: number = readlineSync.question("");
+  console.log(input4);
+
+  for (let i = 0; i < goodContent.length; i++) {
+    if (goodContent[input4 - 1] === input4) {
+      goodContent[input4 - 1].isCompleted = true;
+    }
+  }
+  console.log(goodContent[input4 - 1].isCompleted);
+}
+
 /*else if (input1 === "-r") {
   goodContent.replace(input1,"")
 }*/
