19fd7d745b60698f6018e594317b220d0bb9ede7
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-08 14:24:46 +0100

    Created the base of the first task

--- week-04/day-5/todo-app-main.ts
+++ week-04/day-5/todo-app-main.ts
@@ -1,18 +1,59 @@
 'use strict';
 
 let readlineSync = require('readline-sync');
+let fs = require('fs');
+let content: string = fs.readFileSync("todolist.txt", "utf-8")
+let goodContent: any [] = content.split("\n")
+let taskList: object [] = [];
 
-export let todoItem: string = readlineSync.question(
+function removeChars (array: any) {
+  let something: any [] = [];
+for (let i = 0; i < array.length; i++) {
+something[i] = array[i].replace("\r", "");
+}
+return something;
+}
+goodContent = removeChars(goodContent);
+
+let input1: string = readlineSync.question(
   'Welcome to my todo app, what would you like to add today? '
 );
 
-if (todoItem === '') {
+if (input1 === "") {
   console.log(`Command Line Todo application
   =============================
   
   Command line arguments:
    -l   Lists all the tasks
    -a   Adds a new task
-   -r   Removes an task
-   -c   Completes an task`);
+   (Please add a space and then write your task to be added.)
+   -r   Removes a task
+   -c   Completes a task`);
 }
+
+let input2: string = readlineSync.question(
+  ''
+);
+if (input2 === "-l") {
+  goodContent.forEach(function(item: string,index: number) {
+    console.log(index + " [ ] ", item);
+  })
+}
+else if (input2 === "-a") {
+  let input3: string = readlineSync.question(
+    'Please type your new task to be added to the list:'
+  );
+  goodContent.push(input3);
+  fs.appendFileSync("todolist.txt", "\n" + goodContent);
+}
+/*else if (input2[0] === "-" && input2[1] === "a") {
+ let splitTodo = input2.substring(3);
+ console.log(splitTodo);
+ goodContent.push(splitTodo);
+ console.log(goodContent);
+ fs.appendFileSync("todolist.txt", "\n" + goodContent);
+}*/
+/*else if (input1 === "-r") {
+  goodContent.replace(input1,"")
+}*/
+
