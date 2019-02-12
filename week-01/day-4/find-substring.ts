6e6a273eaf12188ffa5516304bf66be642244de2
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-12 11:59:53 +0100

    Created find substring exercise from week 1.

---
+++ week-01/day-4/find-substring.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+//  Create a function that takes two strings as a parameter
+//  Returns the starting index where the second one is starting in the first one
+//  Returns `-1` if the second string is not in the first one
+
+function substr(str: string, keyword: string) : number {
+  for (let i:number = 0; i < str.length; i++) {
+    if (str.indexOf(keyword) >= 0) {
+      return str.indexOf(keyword);
+    }
+  }
+  return -1;
+};
+
+//  Example
+
+// should print: `17`
+console.log(substr("this is what I'm searching in", "searching"));
+
+// should print: `-1`
+console.log(substr("this is what I'm searching in", "not"));
