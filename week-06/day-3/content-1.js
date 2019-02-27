5e5706934ff9c3bb6a165525cac2a3d4aa35c64e
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-27 14:19:40 +0100

    Some modifications

--- week-06/day-3/content-1.js
+++ week-06/day-3/content-1.js
@@ -1,4 +1,14 @@
 'use strict';
+//This is the first exercise
+var pTagstext = document.getElementsByTagName("p");
+for (var i = 0; i < pTagstext.length; i++) {
+    pTagstext[i].innerText = pTagstext[pTagstext.length - 1].innerText;
+}
+for (var i = 0; i < pTagstext.length; i++) {
+    console.log(pTagstext[i]);
+}
+;
+//This is the second exercise
 var pTags = document.getElementsByTagName("p");
 for (var i = 0; i < pTags.length - 1; i++) {
     pTags[i].innerHTML = pTags[pTags.length - 1].innerHTML;
@@ -6,3 +16,4 @@ for (var i = 0; i < pTags.length - 1; i++) {
 for (var i = 0; i < pTags.length; i++) {
     console.log(pTags[i]);
 }
+;
--- week-06/day-3/content-1.ts
+++ week-06/day-3/content-1.ts
@@ -2,6 +2,17 @@
 
 //This is the first exercise
 
+let pTagstext = document.getElementsByTagName("p");
+
+for (let i: number = 0; i < pTagstext.length; i++) {
+  pTagstext[i].innerText = pTagstext[pTagstext.length-1].innerText;
+}
+
+for (let i: number = 0; i < pTagstext.length; i++) {
+  console.log(pTagstext[i]);
+}; 
+
+//This is the second exercise
 let pTags = document.getElementsByTagName("p");
 
 for (let i: number = 0; i < pTags.length-1; i++) {
