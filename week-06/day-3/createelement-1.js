978babf2399fdb18db07ba3cfba7c357a463c927
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-27 17:23:50 +0100

    Finished create element exercise

--- week-06/day-3/createelement-1.js
+++ week-06/day-3/createelement-1.js
@@ -1,8 +1,19 @@
 'use strict';
+//First task
 var newElement = document.createElement("li");
 var asteroidList = document.getElementsByClassName("asteroids")[0];
 newElement.innerText = "The Green Fox";
 asteroidList.appendChild(newElement);
+//Second task
 var newElement2 = document.createElement("li");
 newElement2.innerText = "The Lamplighter";
 asteroidList.appendChild(newElement2);
+//Third task
+var newHeading = document.createElement("h1");
+var container = document.querySelector(".container");
+newHeading.innerText = "I can add elements to the DOM!";
+container.appendChild(newHeading);
+//Fourth task
+var newImage = document.createElement("img");
+newImage.setAttribute("src", "https://consequenceofsound.files.wordpress.com/2018/09/screen-shot-2018-09-17-at-9-46-16-pm.png?w=471&h=521");
+container.appendChild(newImage);
--- week-06/day-3/createelement-1.ts
+++ week-06/day-3/createelement-1.ts
@@ -10,3 +10,14 @@ asteroidList.appendChild(newElement);
 let newElement2 = document.createElement("li");
 newElement2.innerText = "The Lamplighter";
 asteroidList.appendChild(newElement2);
+
+//Third task
+let newHeading = document.createElement("h1");
+let container = document.querySelector(".container");
+newHeading.innerText = "I can add elements to the DOM!";
+container.appendChild(newHeading);
+
+//Fourth task
+let newImage = document.createElement("img");
+newImage.setAttribute("src", "https://consequenceofsound.files.wordpress.com/2018/09/screen-shot-2018-09-17-at-9-46-16-pm.png?w=471&h=521");
+container.appendChild(newImage);
