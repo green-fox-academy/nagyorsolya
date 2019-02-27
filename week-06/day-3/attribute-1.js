88c3d412dbd6238218d70c2ee77103c2928c2807
Orsi Nagy <orsolya.n91@gmail.com>
2019-02-27 15:30:21 +0100

    Finished forth exercise of attribute-1

---
+++ week-06/day-3/attribute-1.js
@@ -0,0 +1,11 @@
+'use strict';
+//This is the first exercise
+var img = document.querySelector("img");
+console.log(img.getAttribute("src"));
+//This is the second exercise
+img.setAttribute("src", "https://i.ndtvimg.com/i/2017-02/australian-quokko-650_650x400_71488173163.jpg?output-quality=70&output-format=webp");
+//This is the third exercise
+var aTag = document.querySelector("a");
+aTag.setAttribute("href", "http://greenfoxacademy.com");
+//This is the third exercise
+var button1 = document.querySelector(".this-one").setAttribute("disabled", "true");
---
+++ week-06/day-3/attribute-1.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+//This is the first exercise
+
+let img = document.querySelector("img");
+
+console.log(img.getAttribute("src"));
+
+//This is the second exercise
+
+img.setAttribute("src","https://i.ndtvimg.com/i/2017-02/australian-quokko-650_650x400_71488173163.jpg?output-quality=70&output-format=webp");
+
+//This is the third exercise
+
+let aTag = document.querySelector("a");
+
+aTag.setAttribute("href", "http://greenfoxacademy.com");
+
+//This is the third exercise
+
+let button1 = document.querySelector(".this-one").setAttribute("disabled", "true");
+
