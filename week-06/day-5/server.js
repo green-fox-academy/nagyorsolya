e4b51a6bacc497be80f01265031f1dc9c9e50102
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-01 15:32:01 +0100

    Restarted everything, adding progress so far

--- week-06/day-5/server.js
+++ week-06/day-5/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+let images = ['views/first.jpg', 'views/second.jpg', 'views/third.jpg', 'views/fourth.jpg'];
+let buttonleft = 'views/buttonleft.jpg';
 
 app.set("view engine", "ejs");
 app.use("/views", express.static("views"));
 
 app.get("/", (req, res) => {
-  res.render('home')
+  res.render('home', {image: images, button1: buttonleft})
  });
 
 app.listen(PORT, () => {
