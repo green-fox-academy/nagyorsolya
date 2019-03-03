a1e53d28638fb45d81814f42a76c1b71c0f898cc
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-03 19:45:31 +0100

    Updated gallery exercise with a random pic selector button that doesn't show the same picture consequently!

--- week-06/day-5/views/home.js
+++ week-06/day-5/views/home.js
@@ -1,33 +1,59 @@
-'use strict';
+"use strict";
 
-let gallerypics1 = [{path: 'views/masni.jpg', name: "The first picture of Masni.",title: "This is one very lazy dog."}, 
-{path: 'views/masni2.jpg', name: "The second picture of Masni.",title: "This is one very lazy dog."},
-{path: 'views/masni3.jpg', name: "The third picture of Masni.",title: "This is one very lazy dog."},
-{path: 'views/masni4.jpg', name: "The fourth picture of Masni.",title: "This is one very lazy dog."}
+let gallerypics1 = [
+  {
+    path: "views/masni.jpg",
+    name: "The first picture of Masni.",
+    title: "This is one very lazy dog."
+  },
+  {
+    path: "views/masni2.jpg",
+    name: "The second picture of Masni.",
+    title: "This is one very lazy dog."
+  },
+  {
+    path: "views/masni3.jpg",
+    name: "The third picture of Masni.",
+    title: "This is one very lazy dog."
+  },
+  {
+    path: "views/masni4.jpg",
+    name: "The fourth picture of Masni.",
+    title: "This is one very lazy dog."
+  }
 ];
-
 
 let i = 0;
 
-function changeImageAndTitles (picNumber) {
+function changeImageAndTitles(picNumber) {
   i = picNumber;
-  document.getElementById("masnik").setAttribute("src", gallerypics1[i].path)
-  document.getElementsByClassName("galleryname")[0].innerHTML = gallerypics1[i].name;
-  document.getElementsByClassName("gallerytitle")[0].innerHTML = gallerypics1[i].title;
+  document.getElementById("masnik").setAttribute("src", gallerypics1[i].path);
+  document.getElementsByClassName("galleryname")[0].innerHTML =
+    gallerypics1[i].name;
+  document.getElementsByClassName("gallerytitle")[0].innerHTML =
+    gallerypics1[i].title;
 }
 
-function changeImageRight () {
-  i++;  
-  if (i=== gallerypics1.length) {
+function changeImageRight() {
+  i++;
+  if (i === gallerypics1.length) {
     i = 0;
   }
-changeImageAndTitles(i);
-  };
+  changeImageAndTitles(i);
+}
 
-  function changeImageLeft () {
-    i--;  
-    if (i < 0) {
-      i = gallerypics1.length-1;
-    }
-    changeImageAndTitles(i);
-    };
+function changeImageLeft() {
+  i--;
+  if (i < 0) {
+    i = gallerypics1.length - 1;
+  }
+  changeImageAndTitles(i);
+}
+
+function randomPicGenerator() {
+  let newNumber = 0;
+  while (newNumber === i) {
+    newNumber = Math.floor(Math.random() * 4) + 0;
+  }
+    changeImageAndTitles(newNumber);
+}
