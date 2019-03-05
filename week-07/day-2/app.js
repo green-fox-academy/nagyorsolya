26ca7e183de66e2b8927d6baed0ec88262a6a33a
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-05 16:57:02 +0100

    Finished dountil exercise

--- week-07/day-2/app.js
+++ week-07/day-2/app.js
@@ -1,52 +1,78 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8080;
-const path = require('path');
+const path = require("path");
+app.use(express.json()); //middleware
 
-app.use('/assets', express.static('assets'));
+app.use("/assets", express.static("assets"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/doubling', (req, res) => {
-  res.json(    
- req.query.input?{
-    received: Number(req.query.input),
-    result: Number(req.query.input*2)
+app.get("/doubling", (req, res) => {
+  res.json(
+    req.query.input
+      ? {
+          received: Number(req.query.input),
+          result: Number(req.query.input * 2)
+        }
+      : { error: "Please provide an input!" }
+  );
+});
+
+app.get("/greeter", (req, res) => {
+  if (req.query.name && req.query.title) {
+    res.json({
+      welcome_message: `Oh, hi there ${req.query.name}, my dear ${
+        req.query.title
+      }!`
+    });
+  } else if (!req.query.name && !req.query.title) {
+    res.json({ error: "Please provide a name and a title!" });
+  } else if (!req.query.name) {
+    res.json({ error: "Please provide a name!" });
+  } else if (!req.query.title) {
+    res.json({ error: "Please provide a title!" });
   }
-  : {error: 'Please provide an input!'});
 });
 
-app.get('/greeter', (req, res) => {
-    if (req.query.name && req.query.title) {
-    res.json({welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`})
-  }
-    else if (!req.query.name && !req.query.title) {
-      res.json({error: "Please provide a name and a title!"})
-    }
-    else if (!req.query.name) {
-      res.json({error: "Please provide a name!"})
-    }
-    else if (!req.query.title) {
-      res.json({error: "Please provide a title!"})
-    }
-});
-
-app.get('/appenda/:id', (req, res) => {
+app.get("/appenda/:id", (req, res) => {
   if (!req.params.id) {
-    res.send({error: "404"})
+    res.send({ error: "404" });
+  } else {
+    res.json({ appended: req.params.id + "a" });
   }
-  else {
-res.json({appended: req.params.id + "a"})
-  };
 });
 
+app.post("/dountil/:action", (req, res) => {
+  let inputNum = req.params.action;
+  let data = req.body;
+  if (inputNum === "sum") {
+    res.json({ result: sumUntil(data.until) });
+  } else if (inputNum === "factor") {
+    res.json({ result: rFact(data.until) });
+  }
+});
+
+function sumUntil(number) {
+  let resultSum = 0;
+  for (let i = 0; i <= number; i++) {
+    resultSum += i;
+  }
+  return resultSum;
+}
+
+function rFact(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * rFact(num - 1);
+  }
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-
