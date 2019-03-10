ca5841ab51e3847093e8820a172dcd6b49e761d0
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-10 12:53:17 +0100

    Finished bookstore exercise with filter buttons

--- week-07/day-4/assets/js/frontend2.js
+++ week-07/day-4/assets/js/frontend2.js
@@ -1,45 +1,45 @@
 "use strict";
 
-function whatIsThisDoing (url) {
-let httpRequest = new XMLHttpRequest();
-httpRequest.open(
-  "GET",
- url
-);
-httpRequest.onload = () => {
-  let data = JSON.parse(httpRequest.responseText);
-  for (let i = 0; i < data.length; i++ ) {
-    let table = document.getElementById("content");
-    let line = document.createElement("tr");
-    let dataElement = document.createElement("td");
-    dataElement.innerText = data[i].book_name;
-    let dataElement2 = document.createElement("td");
-    dataElement2.innerText = data[i].aut_name;
-    let dataElement3 = document.createElement("td");
-    dataElement3.innerText = data[i].pub_name;
-    let dataElement4 = document.createElement("td");
-    dataElement4.innerText = data[i].cate_descrip;
-    let dataElement5 = document.createElement("td");
-    dataElement5.innerText = data[i].book_price + "$";
-    line.appendChild(dataElement);
-    line.appendChild(dataElement2);
-    line.appendChild(dataElement3);
-    line.appendChild(dataElement4);
-    line.appendChild(dataElement5);
-    table.appendChild(line);
+function createTable (url) {
+  let httpRequest = new XMLHttpRequest();
+  httpRequest.open("GET", url);
+  httpRequest.onload = () => {
+    let data = JSON.parse(httpRequest.responseText);
+    for (let i = 0; i < data.length; i++) {
+      let table = document.getElementById("tablecontent");
+      let line = document.createElement("tr");
+      let dataElement = document.createElement("td");
+      dataElement.innerText = data[i].book_name;
+      let dataElement2 = document.createElement("td");
+      dataElement2.innerText = data[i].aut_name;
+      let dataElement3 = document.createElement("td");
+      dataElement3.innerText = data[i].pub_name;
+      let dataElement4 = document.createElement("td");
+      dataElement4.innerText = data[i].cate_descrip;
+      let dataElement5 = document.createElement("td");
+      dataElement5.innerText = data[i].book_price + "$";
+      line.appendChild(dataElement);
+      line.appendChild(dataElement2);
+      line.appendChild(dataElement3);
+      line.appendChild(dataElement4);
+      line.appendChild(dataElement5);
+      table.appendChild(line);
+    }
   };
-  };
-httpRequest.send();
-};
+  httpRequest.send();
+}
 
-//console.log(httpRequest.responseText);
+createTable("/booklist");
 
-whatIsThisDoing("/booklist");
+createButtonsEventListeners();
 
-  let firstButton = document.getElementById("firstbutton");
-  firstButton.addEventListener("click", function () {
-   let divTag = document.getElementById("wrapper");
-   let table = document.getElementById("content");
-   divTag.removeChild(table);
-    whatIsThisDoing("/booklist?category=Medical");
- }); 
+function createButtonsEventListeners() {
+  let buttons = document.getElementsByClassName("filter");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+      let table = document.getElementById("tablecontent");
+      table.innerHTML = "";
+      createTable("/booklist?category=" + buttons[i].innerText);
+    });
+  }
+}
