b8395a42a4ad17fde66d3a583e480dc1b29c4284
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-10 20:57:04 +0100

    Some modification to prime-squares exercise

--- week-07/day-3/prime-squares.js
+++ week-07/day-3/prime-squares.js
@@ -1,6 +1,5 @@
 'use strict';
 
-let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
 function createDivs () {
 for (let i = 1; i <= 100; i++) {
