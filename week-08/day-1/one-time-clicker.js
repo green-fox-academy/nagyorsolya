fa20d1ab6fc01f5f4d41323ce8bd89130bd3d91b
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-11 11:30:19 +0100

    Finished One time clicker exercise with one solution

---
+++ week-08/day-1/one-time-clicker.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//First solution with add and remove event listener
+
+let button = document.getElementsByTagName("button")[0];
+
+function getDate () {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //January is 0!
+  let yyyy = today.getFullYear();
+
+  today = mm + '/' + dd + '/' + yyyy;
+  console.log(today);
+  button.removeEventListener('click', getDate);
+} 
+
+button.addEventListener('click', getDate);
