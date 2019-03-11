f742fad89c8d0839de404206f50015786aef9e43
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-11 11:57:28 +0100

    Finished One time clicker exercise with another solution

--- week-08/day-1/one-time-clicker.js
+++ week-08/day-1/one-time-clicker.js
@@ -13,6 +13,21 @@ function getDate () {
   today = mm + '/' + dd + '/' + yyyy;
   console.log(today);
   button.removeEventListener('click', getDate);
-} 
+};
 
-button.addEventListener('click', getDate);
+button.addEventListener('click', getDate); 
+
+/* //Second solution with classes
+
+button.addEventListener('click', function () {
+  if (button.className == "clickable") {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0!
+    let yyyy = today.getFullYear();
+  
+    today = mm + '/' + dd + '/' + yyyy;
+    console.log(today);
+    button.className = "";
+  };
+}); */
