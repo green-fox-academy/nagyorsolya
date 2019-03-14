616a9eaaaed4e28aa60479eb1fd51a47d11e8138
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-14 17:48:28 +0100

    Some modifications

--- week-07/day-5/assets/js/frontpage.js
+++ week-07/day-5/assets/js/frontpage.js
@@ -24,7 +24,7 @@ httpRequest.onload = () => {
     iconAndScore.appendChild(imageup);
     let score = document.createElement("p");
     score.innerText = data[i].score;
-    score.setAttribute("id", data[i].post_id)
+    score.setAttribute("id", data[i].post_id);
     iconAndScore.appendChild(score);
     let imagedown = document.createElement("img");
     imagedown.setAttribute("class", "downvote");
@@ -40,43 +40,38 @@ httpRequest.onload = () => {
     textElements.appendChild(postTitle);
     let postModify = document.createElement("a");
     postModify.innerText = "Modify Remove";
-    postModify.setAttribute("href", "#")
+    postModify.setAttribute("href", "#");
     textElements.appendChild(postModify);
-
-
-
   }
   let images = document.getElementsByTagName("img");
   for (let i = 0; i < images.length; i++) {
     let imageID = images[i].getAttribute("id");
     if (images[i].className === "upvote") {
-    images[i].addEventListener('click', function () {
-      let httpRequest2 = new XMLHttpRequest();
-      httpRequest2.open("PUT", `/posts/${imageID}/upvote`);
-      httpRequest2.send();
-      httpRequest2.onload = () => {
-      let response = JSON.parse(httpRequest2.responseText);
-      console.log(response);
-      let currentPost = document.querySelector(`p[id="${imageID}"]`);
-      currentPost.innerText = response[0].score;
-      }
-    });
-  }
-  if (images[i].className === "downvote") {
-    images[i].addEventListener('click', function () {
-      let imageID = images[i].getAttribute("id");
-      let httpRequest2 = new XMLHttpRequest();
-      httpRequest2.open("PUT", `/posts/${imageID}/downvote`);
-      httpRequest2.send();
-      httpRequest2.onload = () => {
-      let response = JSON.parse(httpRequest2.responseText);
-      console.log(response);
-      let currentPost = document.querySelector(`p[id="${imageID}"]`);
-      currentPost.innerText = response[0].score;
-      }
-    });
-  }
-
+      images[i].addEventListener("click", function() {
+        let httpRequest2 = new XMLHttpRequest();
+        httpRequest2.open("PUT", `/posts/${imageID}/upvote`);
+        httpRequest2.send();
+        httpRequest2.onload = () => {
+          let response = JSON.parse(httpRequest2.responseText);
+          console.log(response);
+          let currentPost = document.querySelector(`p[id="${imageID}"]`);
+          currentPost.innerText = response[0].score;
+        };
+      });
+    }
+    if (images[i].className === "downvote") {
+      images[i].addEventListener("click", function() {
+        let httpRequest2 = new XMLHttpRequest();
+        httpRequest2.open("PUT", `/posts/${imageID}/downvote`);
+        httpRequest2.send();
+        httpRequest2.onload = () => {
+          let response = JSON.parse(httpRequest2.responseText);
+          console.log(response);
+          let currentPost = document.querySelector(`p[id="${imageID}"]`);
+          currentPost.innerText = response[0].score;
+        };
+      });
+    }
   }
 };
 
