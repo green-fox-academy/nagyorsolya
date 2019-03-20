8339553bda31fe5cd5734c480a05421309c07362
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-20 11:53:46 +0100

    Finished reject a promise exercise

---
+++ week-09/day-3/promise-shop/reject.js
@@ -0,0 +1,17 @@
+  'use strict';
+require('es6-promise');
+  
+   var promise = new Promise(function (fulfill, reject) {
+      // Your solution here
+      setTimeout(() => {
+        reject(new Error('REJECTED!'));
+      }, 300);
+    });
+
+    function onReject (error) {
+      // Your solution here
+      console.log(error.message);
+    }
+    
+    // Your solution here
+    promise.then(null, onReject);
