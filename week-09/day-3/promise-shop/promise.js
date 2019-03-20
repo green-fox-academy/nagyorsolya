a771699c72bea61cf247a11eee241b95d1f12859
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-20 11:34:27 +0100

    Created Warm up and Fulfill a promise exercises

---
+++ week-09/day-3/promise-shop/promise.js
@@ -0,0 +1,14 @@
+'use strict';
+require('es6-promise');
+
+    var promise = new Promise(function (fulfill, reject) {
+      // Your solution here
+      setTimeout(() => {
+        fulfill('FULFILLED!');
+      }, 300);
+    });
+
+    // Your solution here
+    promise.then(function(value){
+      console.log(value);
+    });
