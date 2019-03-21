2b3d845831af5b2270f33905f053a04ba711c23a
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-21 16:09:37 +0100

    Created js file to render random questions on gamepage.html and added onclick event to color the answers' background color

--- week-09/day-4/assets/js/gamepage.js
+++ week-09/day-4/assets/js/gamepage.js
@@ -1,25 +1,40 @@
-'use strict';
+"use strict";
 
-  let httprequest = new XMLHttpRequest();
-  httprequest.open('GET', '/api/game');
-  httprequest.onload = () => {
-    let data = JSON.parse(httprequest.responseText);
+let httprequest = new XMLHttpRequest();
+httprequest.open("GET", "/api/game");
+httprequest.onload = () => {
+  let data = JSON.parse(httprequest.responseText);
 
+  let questionTitle = document.getElementsByTagName("h3")[0];
+  questionTitle.innerText = data.question;
+  createElement("answer1", 0, data);
+  createElement("answer2", 1, data);
+  createElement("answer3", 2, data);
+  createElement("answer4", 3, data);
 
-    let questionTitle = document.getElementsByTagName("h3")[0];
-    questionTitle.innerText = data.question;
+  let divTags = document.querySelectorAll("div");
+  console.log(divTags);
+  divTags.forEach(element => {
+    console.log(element);
+    element.addEventListener("click", function() {
+      console.log(element.getAttribute("iscorrect"));
+      if (element.getAttribute("iscorrect") === "0") {
+        element.style.backgroundColor = "red";
+      } else {
+        element.style.backgroundColor = "green";
+      }
+    });
+  });
+};
 
-    createElement("question1", 0, data);
-    createElement( "question2", 1, data);
-    createElement("question3", 2, data);
-    createElement("question4", 3, data);
-    
-    
+httprequest.send();
+
+function createElement(id, number, data) {
+  let name = document.getElementById(id);
+  name.innerText = data.answers[number].answer;
+  if (data.answers[number].is_correct === 0) {
+    name.setAttribute("iscorrect", 0);
+  } else {
+    name.setAttribute("iscorrect", 1);
   }
-
-  httprequest.send();
-
-  function createElement (id, number, data) {
-    let name = document.getElementById(id);
-    name.innerText = data.answers[number].answer;
-  }
+}
