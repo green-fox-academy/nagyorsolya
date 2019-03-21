b17778ec1afc0d1b5aaeb047f4b17ed3304b3657
Orsi Nagy <orsolya.n91@gmail.com>
2019-03-21 16:38:27 +0100

    Finished rendering the game page with new random questions, only allowing one click and coloring the answers

--- week-09/day-4/assets/js/gamepage.js
+++ week-09/day-4/assets/js/gamepage.js
@@ -3,6 +3,7 @@
 let httprequest = new XMLHttpRequest();
 httprequest.open("GET", "/api/game");
 httprequest.onload = () => {
+  let score = 0;
   let data = JSON.parse(httprequest.responseText);
 
   let questionTitle = document.getElementsByTagName("h3")[0];
@@ -13,15 +14,39 @@ httprequest.onload = () => {
   createElement("answer4", 3, data);
 
   let divTags = document.querySelectorAll("div");
-  console.log(divTags);
+  let numberOfclicks = 0;
   divTags.forEach(element => {
-    console.log(element);
     element.addEventListener("click", function() {
-      console.log(element.getAttribute("iscorrect"));
-      if (element.getAttribute("iscorrect") === "0") {
-        element.style.backgroundColor = "red";
-      } else {
-        element.style.backgroundColor = "green";
+      numberOfclicks++;
+      if (numberOfclicks <= 1) {
+        if (element.getAttribute("iscorrect") === "0") {
+          element.style.backgroundColor = "red";
+          setTimeout(() => {
+            let goodAnswer = document.querySelector("div[iscorrect='1']");
+            goodAnswer.style.backgroundColor = "green";
+          }, 1000);
+        } else {
+          element.style.backgroundColor = "green";
+          score++;
+          let scoreTracker = document.getElementById("score");
+          scoreTracker.innerText = `Score: ${score}`;
+        }
+        setTimeout(() => {
+          let loadNewGame = new XMLHttpRequest();
+          loadNewGame.open("GET", "/api/game");
+          loadNewGame.onload = () => {
+            let data = JSON.parse(loadNewGame.responseText);
+
+            let questionTitle = document.getElementsByTagName("h3")[0];
+            questionTitle.innerText = data.question;
+            createElement("answer1", 0, data);
+            createElement("answer2", 1, data);
+            createElement("answer3", 2, data);
+            createElement("answer4", 3, data);
+            numberOfclicks = 0;
+          };
+          loadNewGame.send();
+        }, 3000);
       }
     });
   });
@@ -32,6 +57,7 @@ httprequest.send();
 function createElement(id, number, data) {
   let name = document.getElementById(id);
   name.innerText = data.answers[number].answer;
+  name.style.backgroundColor = "blue";
   if (data.answers[number].is_correct === 0) {
     name.setAttribute("iscorrect", 0);
   } else {
