3dd5cdc05179244df8f23ba4a43735caf747b343
Orsi Nagy <orsolya.n91@gmail.com>
2019-04-02 17:11:35 +0200

    Created static weather forecast components

--- First-angular-project/first-angular-project/src/app/app.module.ts
+++ First-angular-project/first-angular-project/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeattleComponent } from './seattle/seattle.component';
+import { MiamiComponent } from './miami/miami.component';
+import { BarcelonaComponent } from './barcelona/barcelona.component';
+import { LondonComponent } from './london/london.component';
+import { BudapestComponent } from './budapest/budapest.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SeattleComponent
+    SeattleComponent,
+    MiamiComponent,
+    BarcelonaComponent,
+    LondonComponent,
+    BudapestComponent
   ],
   imports: [
     BrowserModule,
