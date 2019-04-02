9b9f3c9e9b1d83cade851466396878e57dddab4e
Orsi Nagy <orsolya.n91@gmail.com>
2019-04-02 16:53:10 +0200

    Created first static city component

--- First-angular-project/first-angular-project/src/app/app.module.ts
+++ First-angular-project/first-angular-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeattleComponent } from './seattle/seattle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeattleComponent
   ],
   imports: [
     BrowserModule,
