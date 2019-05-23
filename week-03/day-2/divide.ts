1ce777c6196235460da1c59898c4bdaab2d7bc1a
Orsi Nagy <orsolya.n91@gmail.com>
2019-05-23 09:00:43 +0200

    Corrected divide by 0 task

--- week-03/day-2/divide.ts
+++ week-03/day-2/divide.ts
@@ -3,12 +3,15 @@
 // and prints the result.
 // It should print 'fail' if the parameter is 0
 
-function divideNumber (a) {
-
-  10 / a;
-  if (a === 0) {
-    console.log('fail');
+function divideNumber(a) {
+  try {
+    if (a === 0) {
+      throw Error("Parameter can't be 0");
+    }
+  } catch (e) {
+    console.log('Error: ' + e.message);
   }
-};
+  return 10 / a;
+}
 
 divideNumber(0);
